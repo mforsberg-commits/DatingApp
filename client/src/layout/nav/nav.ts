78c3163e266bc93d1054911a4e0fbfb84b48839b
forsbergmaria <83494435+forsbergmaria@users.noreply.github.com>
2026-03-02 16:58:19 +0100

    feat(client): implement registration and home components; enhance user authentication and display features

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -9,17 +9,15 @@ import { AccountService } from '../../core/services/account-service';
   styleUrl: './nav.css',
 })
 export class Nav {
-  private accountService = inject(AccountService);
+  protected accountService = inject(AccountService);
   protected creds: any = {
 
   }
-  protected loggedIn = signal(false);
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: result => {
         console.log(result)
-        this.loggedIn.set(true);
         this.creds = {};
       },
       error: error => alert(error.message)
@@ -27,6 +25,6 @@ export class Nav {
   }
 
   logout() {
-    this.loggedIn.set(false);
+    this.accountService.logout();
   }
 }
